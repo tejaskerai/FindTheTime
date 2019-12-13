4a9a831658e7358e712163f7e8c36f3412707fca
Tejas Kerai <tejas_444@hotmail.co.uk>
2019-12-13 13:19:31 +0000

    Added home page when sign in is authorised and clicked

--- ms-identity-android-java-master/app/src/main/java/com/azuresamples/findthetime/B2CConfiguration.java
+++ ms-identity-android-java-master/app/src/main/java/com/azuresamples/findthetime/B2CConfiguration.java
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package com.azuresamples.msalandroidapp;
+package com.azuresamples.findthetime;
 
 import java.util.Arrays;
 import java.util.List;
