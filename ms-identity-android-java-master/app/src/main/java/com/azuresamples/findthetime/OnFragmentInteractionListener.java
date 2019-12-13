834bfb45e520d8e47450c48f3bf9e638eb64b709
Tejas Kerai <tejas_444@hotmail.co.uk>
2019-12-13 13:19:31 +0000

    Added home page when sign in is authorised and clicked

--- ms-identity-android-java-master/app/src/main/java/com/azuresamples/findthetime/OnFragmentInteractionListener.java
+++ ms-identity-android-java-master/app/src/main/java/com/azuresamples/findthetime/OnFragmentInteractionListener.java
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package com.azuresamples.msalandroidapp;
+package com.azuresamples.findthetime;
 
 /**
  * This interface must be implemented by activities that contain this
