fc43fddd03b905fd6c8162c5e12bc248dee47506
Tejas Kerai <tejas_444@hotmail.co.uk>
2019-12-15 21:22:43 +0000

    Working, minor changes

--- app/src/main/java/com/example/findthetime/Homepage.java
+++ app/src/main/java/com/example/findthetime/Homepage.java
@@ -1,20 +1,25 @@
 package com.example.findthetime;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.microsoft.identity.client.IPublicClientApplication;
 import com.microsoft.identity.client.ISingleAccountPublicClientApplication;
+import com.microsoft.identity.client.PublicClientApplication;
 import com.microsoft.identity.client.exception.MsalException;
 
 public class Homepage extends AppCompatActivity {
 
     private ISingleAccountPublicClientApplication mSingleAccountApp;
     Button signOutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,16 @@ public class Homepage extends AppCompatActivity {
 
 
         signOutButton = findViewById(R.id.btn_signOut);
-
-
         // TODO: Add sign out function from SingleAccount mode
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                System.out.println("Sign out clicked");
+//                Intent signOut = new Intent(v.getContext(), SingleAccountModeFragment.class);
+//                startActivity(signOut);
+            }
+        });
+
 
     }
 }
