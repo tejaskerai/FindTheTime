b9ece01b476182eff2c2261cd8b4efe8beb4422f
Tejas Kerai <tejas_444@hotmail.co.uk>
2019-12-16 15:38:22 +0000

    Removed fragment
    help(comment): Added sign out helper comments

--- app/src/main/java/com/example/findthetime/Homepage.java
+++ app/src/main/java/com/example/findthetime/Homepage.java
@@ -28,16 +28,25 @@ public class Homepage extends AppCompatActivity {
 
 
         signOutButton = findViewById(R.id.btn_signOut);
-        // TODO: Add sign out function from SingleAccount mode
+        // TODO: Add sign out function from main activity
+        // Use mSingleAccountApp.signOut() to sign the user out
+        // You may also be required to redirect user back to the signin page manually
+        // Call mSingleAccountApp.signOut() from a separate thread
+    
 
-        signOutButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(final View v) {
-                System.out.println("Sign out clicked");
-//                Intent signOut = new Intent(v.getContext(), SingleAccountModeFragment.class);
-//                startActivity(signOut);
-            }
-        });
+    }
 
+    /**
+     * Display the error message
+     */
+    private void displayError(@NonNull final Exception exception) {
+        System.out.println("Error occurred in sign out");
+    }
 
+    private void SignOut() {
+
+    }
+    public void update(){
+        System.out.println("sign out from update");
     }
 }
