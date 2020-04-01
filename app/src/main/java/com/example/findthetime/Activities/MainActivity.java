bfdddeffacc17de9f5f72bd3cb56a6e297e2b1e3
Tejas Kerai <tejas_444@hotmail.co.uk>
2020-04-01 17:04:26 +0100

    User is now able to invite people to an activity

--- app/src/main/java/com/example/findthetime/Activities/MainActivity.java
+++ app/src/main/java/com/example/findthetime/Activities/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.findthetime.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.backendless.Backendless;
